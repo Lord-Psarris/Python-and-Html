18faa7e1cdb592999d6a9b1309d66d1778119eb0
Lord-Psarris <theorchestra03@gmail.com>
2021-03-03 05:53:40 +0100

    Changes made

--- Tees/main.py
+++ Tees/main.py
@@ -14,20 +14,6 @@ app = Flask(__name__)
 app.secret_key = 'The_talking_tees'
 
 
-class PageResult:
-    def __init__(self, data, page=1, number=20):
-        self.__dict__ = dict(zip(['data', 'page', 'number'], [data, page, number]))
-        self.full_listing = [self.data[i:i + number] for i in range(0, len(self.data), number)]
-
-    def __iter__(self):
-        for i in self.full_listing[self.page - 1]:
-            yield i
-
-    def __repr__(self):  # used for page linking
-        return "/shop/{}".format(self.page + 1)  # view the next page
-
-
-
 @app.route("/sign-up", methods=["POST", "GET"])
 def sign_up():
     if request.method == "POST":
